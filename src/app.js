9b614f69022e2a95844ece933c09b001f12755b3
Nav gabrie <navil.gabrie@gmail.com>
2021-11-29 21:05:41 -0500

    saving changes

---
+++ src/app.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  let domainName = [];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainName.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+  document.getElementById("domain.results").innerHTML = domainName;
+
+
+
+};
